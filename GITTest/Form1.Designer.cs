af0b4f048fe7389242a457727b5a460a6c183226
p16186227 <p16186227@GWHXX582WIN018W>
2018-02-06 09:16:37 +0000

    .

--- GITTest/Form1.Designer.cs
+++ GITTest/Form1.Designer.cs
@@ -32,7 +32,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lblBeth = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -73,21 +73,21 @@
             this.label3.TabIndex = 3;
             this.label3.Text = "BranchTest";
             // 
-            // label4
+            // lblBeth
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(65, 223);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(28, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "beth";
+            this.lblBeth.AutoSize = true;
+            this.lblBeth.Location = new System.Drawing.Point(65, 223);
+            this.lblBeth.Name = "lblBeth";
+            this.lblBeth.Size = new System.Drawing.Size(28, 13);
+            this.lblBeth.TabIndex = 4;
+            this.lblBeth.Text = "beth";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(351, 319);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblBeth);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
@@ -106,7 +106,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblBeth;
     }
 }
 
